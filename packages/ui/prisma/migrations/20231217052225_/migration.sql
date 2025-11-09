75bc9c4698a0579b9c6a48801047567e611598e9
Alexandr Adamant <alexandradamant@gmail.com>
2025-11-09 15:45:24 +0700

    prisma refactoring

--- packages/ui/prisma/migrations/20231217052225_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `league` to the `Match` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Match" ADD COLUMN     "league" INTEGER NOT NULL;
