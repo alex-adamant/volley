3c843ef2c5b81559e7b5ef25f305f97d6394736d
Alexandr Adamant <alexandradamant@gmail.com>
2025-11-09 23:47:38 +0700

    fix

--- packages/bot/src/utils/redis.ts
+++ packages/bot/src/utils/redis.ts
@@ -3,3 +3,6 @@ import Redis from "ioredis";
 const REDIS_URL = process.env.REDIS_URL!;
 
 export const redis = new Redis(REDIS_URL);
+
+redis.on("connect", () => console.log("✅ Redis connected"));
+redis.on("error", (e) => console.error("❌ Redis error:", e.message));
